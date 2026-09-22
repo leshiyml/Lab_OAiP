ad99adb26e5fb31a5faea50034d0aad986c53a00
leshiyml <leshiyml23@gmail.com>
2026-09-22 17:57:16 +0300

    чуть переделал 3 лабу а также 4

--- lab3_10/lab3_10_2/lab3_10_2.cpp
+++ lab3_10/lab3_10_2/lab3_10_2.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	double a, b, h, n, x, y, k, sum = 0.0, step = 0.0, fakt, i;
+	double a, b, h, n, x, y, k, sum = 0.0, step = 0.0, fakt;
 
 	cout << "Задайте a = ";
 	while (!(cin >> a))
@@ -44,15 +44,12 @@ int main()
 	{
 		double r = 1.0;
 		sum = 1.0;
-		y = 0;
-		int i = 0;
 
 		y = (exp(x) + exp(-x)) / 2;
 		
 		for (k = 1; k < n; k++)
 		{
-			i = i + 1;
-			r = r * x / i;
+			r = r * (x * x) / (2 * k * (2 * k - 1));
 
 			sum += r;
 		}
